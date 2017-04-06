005000443ccf0439a7e66d39e515497a20c2cc57
Yoram Kornatzky <kornatzky@gmail.com>
2017-04-06 11:19:33 +0300

    Test for Functions

--- test/cli_test.js
+++ test/cli_test.js
@@ -134,7 +134,7 @@ describe("lambda .NET", function(done){
 });
 
 
-describe.only("lambda init and deploy", function(done){
+describe("lambda init and deploy", function(done){
 
 	var lambdaUrl = null;
 
@@ -241,4 +241,110 @@ describe.only("lambda init and deploy", function(done){
 	});
 })
 
+describe.only("function init and deploy", function(done){
+
+	var functionUrl = null;
+
+	before(function(done){
+		this.timeout(64000);
+		del.sync(['testclifunction', '*.zip', '.awspublish-nodejs.backand.io', '.backand-credentials.json']);
+		var commandFunctionDelete = '../node_modules/backand/bin/backand function delete --name testclifunction --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 757e33ac-ad5a-11e5-be83-0ed7053426cb  --app cli';	
+		exec(commandFunctionDelete, function(err, stdout, stderr) {	
+			done(); 
+		});
+	});
+
+	it("function init", function(done){
+		this.timeout(64000);
+		var commandFunctionInit = '../node_modules/backand/bin/backand function init --name testclifunction --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 757e33ac-ad5a-11e5-be83-0ed7053426cb  --app cli --template template';	
+		exec(commandFunctionInit, function(err, stdout, stderr) {			
+			var lines = stdout.split('\n');
+			functionUrl = _.find(stdout.split('\n'), function(s) { return _.startsWith(s, 'The function was deployed and can be tested at '); }).replace(/The function was deployed and can be tested at /, '');
+			// test files exist		
+			fs.readdir('testclifunction', (err, files) => {
+				var functionFiles = ['debug.js', 'handler.js', 'index.js', 'package.json'];
+				expect(Array.isArray(files)).to.be.true;
+				var theSame = _.difference(files, functionFiles).length == 0 && _.difference(functionFiles, files).length == 0;
+				expect(theSame).to.be.true;
+				done(); 
+			});
+			 	    
+		});
+	});
+
+	it("function exists", function(done){
+		this.timeout(64000);	
+		request.get('https://api.backand.com/1/businessRule?filter=[{fieldName:"Name",operator:"equals",value:"testclifunction"}]', 
+        	{
+        		auth: {
+					'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
+					'pass': '757e33ac-ad5a-11e5-be83-0ed7053426cb'
+				} 	
+        	},
+        	function(err, response, body){
+        		var bodyObj = JSON.parse(body);
+        		expect(functionUrl).to.be.equal('https://www.backand.com/apps/#/app/cli/function/' + bodyObj.data[0].iD + '/true');
+        		done();        	
+        	}
+        );
+	});
+
+	it("function call", function(done){
+		this.timeout(64000);
+		request.get('https://api.backand.com/1/function/general/testclifunction?parameters={}', 
+        	{
+        		auth: {
+					'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
+					'pass': '757e33ac-ad5a-11e5-be83-0ed7053426cb'
+				} 	
+        	},
+        	function(err, response, body){
+        		expect(err).to.be.null;
+        		var bodyObj = JSON.parse(body);
+        		expect(bodyObj.StatusCode).to.be.equal(200);
+        		expect(bodyObj.Payload).to.be.equal('{"message":"Hello World!"}');
+        		done();        	
+        	}
+        );
+
+	});
+
+	it("function deploy", function(done){
+		this.timeout(64000);
+		var r = Math.random();
+		var options = {
+			files: 'testclifunction/index.js',
+			from: /var helloWorld = \{"message": "Hello World!"\}/g,
+  			to: 'var helloWorld = {"message": "Hello ' + r + '!"}',
+		};
+		replace.sync(options);
+		var commandActionDeploy = '../node_modules/backand/bin/backand function deploy --name testclifunction --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 757e33ac-ad5a-11e5-be83-0ed7053426cb  --app cli --folder testclifunction';	
+		exec(commandActionDeploy, function(err, stdout, stderr) {
+			request.get('https://api.backand.com/1/function/general/testclifunction?parameters={}', 
+	        	{
+	        		auth: {
+						'user': 'b83f5c3d-3ed8-417b-817f-708eeaf6a945',
+						'pass': '757e33ac-ad5a-11e5-be83-0ed7053426cb'
+					} 	
+	        	},
+	        	function(err, response, body){
+	        		expect(err).to.be.null;
+	        		var bodyObj = JSON.parse(body);
+	        		expect(bodyObj.StatusCode).to.be.equal(200);
+	        		expect(bodyObj.Payload).to.be.equal('{"message":"Hello ' + r + '!"}');
+	        		done();        	
+	        	}
+	        );		
+		});
+	});
+
+	after(function(done){
+		this.timeout(64000);
+		del.sync(['testclifunction', '*.zip', '.awspublish-nodejs.backand.io', '.backand-credentials.json']);
+		var commandActionDelete = '../node_modules/backand/bin/backand function delete --name testclifunction --master b83f5c3d-3ed8-417b-817f-708eeaf6a945 --user 757e33ac-ad5a-11e5-be83-0ed7053426cb  --app cli';	
+		exec(commandActionDelete, function(err, stdout, stderr) {
+			done(); 
+		});
+	});
+})
 
